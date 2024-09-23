9466ecb5130d12b4fd68005af6498e4f2e686d6a
nefc42 <32312170+nefc42@users.noreply.github.com>
2024-09-24 08:34:50 +1200

    loaded spark cellphone tower data

---
+++ 01_load_clean_spark_NF.R
@@ -0,0 +1,7 @@
+
+library(tidyverse)
+
+#-------------------------------------------------------------------------------
+#
+sp_data <-  read_csv("C:/rfiles/DATA422/Project/sp_data.csv.gz")
+head(as.data.frame(sp_data), 20)
