3f5218b2817fecab4841c95596abfb7f83cf426d
nefc42 <32312170+nefc42@users.noreply.github.com>
2024-09-24 09:07:54 +1200

    corrected loading sa2

--- 00_load_clean_concordances_NF.R
+++ 00_load_clean_concordances_NF.R
@@ -3,11 +3,12 @@ library(tidyverse)
 getwd()
 list.files("C:/rfiles/DATA422/Project")
 
-
+#-------------------------------------------------------------------------------
+## LOAD IN FILES
 #-------------------------------------------------------------------------------
 # Classification Report: Statistical Area 2 2023
 sa2 <- read_csv("C:/rfiles/DATA422/Project/sa2_2023.csv",
-                skip = 6,
+                skip = 7,
                 col_names = c("sa2_code", "sa2_descriptor")
                 )
 head(sa2)
@@ -36,11 +37,16 @@ sa2_to_ur <- read_csv("C:/rfiles/DATA422/Project/urban_rural_to_sa2_concord_2023
                       col_names = c("sa2_code", "sa2_descriptor", "Mapping", "ur_code","ur_descriptor"))
 head(sa2_to_ur)
 
-
 #-------------------------------------------------------------------------------
-# Sub-national Population Estimates
+## CHECK DATA
+#-------------------------------------------------------------------------------
 
-pop <- read_csv("C:/rfiles/DATA422/Project/subnational_pop_ests.csv") %>%
-  rename(year = `Year at 30 June`)
-head(pop)
+summary(sa2)
+sa2 %>% count(sa2_code) # 2396 unique values, so no duplicates
 
+na_rows <- sa2 %>%
+  filter(is.na(sa2_code))
+na_rows
+
+sa2$sa2_code <- as.integer(sa2$sa2_code)
+head(sa2)
