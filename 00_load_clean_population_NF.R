99ed23ed4405395288bf4390f784070971a177ba
nefc42 <32312170+nefc42@users.noreply.github.com>
2024-09-24 08:56:55 +1200

    loaded population data

---
+++ 00_load_clean_population_NF.R
@@ -0,0 +1,10 @@
+
+library(tidyverse)
+
+#-------------------------------------------------------------------------------
+# Sub-national Population Estimates
+
+pop <- read_csv("C:/rfiles/DATA422/Project/subnational_pop_ests.csv") %>%
+  rename(year = `Year at 30 June`)
+head(pop)
+adfads
