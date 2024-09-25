6e904f4204492296a241953742858267212e31ea
nefc42 <32312170+nefc42@users.noreply.github.com>
2024-09-26 06:47:49 +1200

    rearranged order

--- 01_load_clean_spark_NF.R
+++ 01_load_clean_spark_NF.R
@@ -10,12 +10,13 @@ library(tidyverse)
 
 spark <-  read_csv("C:/rfiles/DATA422/Project/sp_data.csv.gz")
 head(as.data.frame(spark), 20)
+# [811,776 x 3]
 
 spark_clean <- spark %>%
-  rename(timestamp = "ts", sa2_code = "sa2", spark_pings = "cnt")
+  rename(time_stamp = "ts", sa2_code = "sa2", spark_devices = "cnt")
 head(spark_clean)
+str(spark_clean)
 
-# [811,776 x 3]
 
 #-------------------------------------------------------------------------------
 # Check for duplicates
@@ -25,7 +26,9 @@ head(spark_clean)
 
 duplicate_count <- spark_clean %>%
   filter(duplicated(.)) %>%
-  nrow() # 6021 duplicate rows
+  nrow()
+print(duplicate_count)
+# 6021 duplicate rows
 
 
 # Remove duplicates
@@ -34,7 +37,6 @@ spark_clean <- spark_clean %>%
   distinct()
 
 str(spark_clean)
-
 # [805,755 × 3]
 
 #-------------------------------------------------------------------------------
@@ -45,14 +47,13 @@ na_columns <- spark_clean %>%
   map_lgl(~ any(is.na(.))) %>%
   enframe(name = "column", value = "has_na") %>%
   filter(has_na)
-
 print(na_columns)
 
 # so yes, there are NA values. How many?
 
 na_count <- spark_clean %>%
   summarise(
-    na_count = sum(is.na(spark_pings)),
+    na_count = sum(is.na(spark_devices)),
     total_count = n(),
     na_percentage = (na_count / total_count) * 100
     )
