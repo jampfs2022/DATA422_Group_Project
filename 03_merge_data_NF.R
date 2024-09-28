1f3360826603a4d36b83b49d99699032b1b36655
nefc42 <32312170+nefc42@users.noreply.github.com>
2024-09-29 12:46:22 +1300

    corrected vodafone spelling

--- 03_merge_data_NF.R
+++ 03_merge_data_NF.R
@@ -1,6 +1,7 @@
 
 source("00_load_clean_supplementary_data.R")
-source("01_load_clean_spark_NF.R")
-source("02_load_clean_vodafone_NF.R")
+source("01_load_clean_vodafone_NF.R")
+source("02_load_clean_spark_NF.R")
+
 
 
