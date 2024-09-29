69720976d615035813a2f46064e2a43288988959
nefc42 <32312170+nefc42@users.noreply.github.com>
2024-09-29 17:51:51 +1300

    function convert midnight cell counts to population with first guess algorithm

--- 04_algorithm_NF.R
+++ 04_algorithm_NF.R
@@ -1,5 +1,5 @@
 
-
+# if data were individual numbers ...
 convert_to_population <- function(sprint, vodafone, num_adults, num_kids) {
 
   # set c3 as mean of the other two:
@@ -7,4 +7,19 @@ convert_to_population <- function(sprint, vodafone, num_adults, num_kids) {
   population = (sprint + vodafone + third_provider) * (1.2 * num_adults + 0.1 * num_kids )
 
     return(population)
+}
+
+#------------------------------------------------------------------------------
+# now write in dyplr for df manipulation
+
+# set c3 as mean of the other two:
+
+convert_to_population <- function(df_midnight) {
+
+  midnight_population %>% df_midnight
+    mutate(third_provider = (spark_devices + vodafone_devices) / 2) %>%
+    mutate(adult_count = (total_count - child_count)) %>%
+    mutatue(population = (sprint_devices + vodafone_devices + third_provider * (1.2 * adult_count + 0.1 * child_count)))
+
+  return(midnight_population)
 }
