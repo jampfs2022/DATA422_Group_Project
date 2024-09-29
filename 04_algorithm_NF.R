7c17915c605d74e5de71579af719092edde9a84f
nefc42 <32312170+nefc42@users.noreply.github.com>
2024-09-29 18:12:33 +1300

    start on basic algorithm

--- 04_algorithm_NF.R
+++ 04_algorithm_NF.R
@@ -1,16 +1,27 @@
+library(tidyverse)
+library(summarytools)
+
+#-------------------------------------------------------------------------------
+# load data
+#-------------------------------------------------------------------------------
+
+df_loaded <- readRDS("df_final.rds")
+
+#-------------------------------------------------------------------------------
+# extract counts from midnight - 1am
+#-------------------------------------------------------------------------------
+# need to justify choice of time to use - plot to see patterns
+# looking for the night time drop off, but not too late in night that inactive cell phones
+# are dropped from tower
+
+df_midnight <- df_loaded %>%
+  filter(hour(time_stamp) == 0)
 
 #------------------------------------------------------------------------------
-# now write in dyplr for df manipulation
-
-# set c3 as mean of the other two:
-
-convert_to_population <- function(df_midnight) {
 
   midnight_population <- df_midnight %>%
-    mutate(third_provider = (spark_devices + vodafone_devices) / 2) %>%
+    mutate(other_devices = (spark_devices + vodafone_devices) / 2) %>%
     mutate(adult_count = (total_count - child_count)) %>%
-    mutate(population = (spark_devices + vodafone_devices + third_provider) *
+    mutate(pop_estimate = (spark_devices + vodafone_devices + third_provider) *
               (1.2 * adult_count + 0.1 * child_count))
 
-  return(midnight_population)
-}
