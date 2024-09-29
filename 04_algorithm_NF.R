c6c3df8eb0e390a0d379eea6bde0bd0ef7f82df0
nefc42 <32312170+nefc42@users.noreply.github.com>
2024-09-29 18:38:06 +1300

    great results using 1.2 for adults, 0.1 for kids

--- 04_algorithm_NF.R
+++ 04_algorithm_NF.R
@@ -6,6 +6,7 @@ library(summarytools)
 #-------------------------------------------------------------------------------
 
 df_loaded <- readRDS("df_final.rds")
+str(df_loaded)
 
 #-------------------------------------------------------------------------------
 # extract counts from midnight - 1am
@@ -15,13 +16,16 @@ df_loaded <- readRDS("df_final.rds")
 # are dropped from tower
 
 df_midnight <- df_loaded %>%
-  filter(hour(time_stamp) == 0)
+  filter(hour(time_stamp) == 0) %>%
+  mutate(total_devices = spark_devices + vodafone_devices) %>%
+  mutate(adult_count = total_count - child_count) %>%
+  mutate(population_weight = (1.2 * (adult_count / (adult_count + child_count)) +
+                                0.1 * (child_count / (adult_count + child_count)))) %>%
+  mutate(estimated_population = total_devices / population_weight) %>%
+  select(total_devices, adult_count, child_count, total_count, estimated_population)
+
+head(df_midnight)
 
 #------------------------------------------------------------------------------
 
-  midnight_population <- df_midnight %>%
-    mutate(other_devices = (spark_devices + vodafone_devices) / 2) %>%
-    mutate(adult_count = (total_count - child_count)) %>%
-    mutate(pop_estimate = (spark_devices + vodafone_devices + third_provider) *
-              (1.2 * adult_count + 0.1 * child_count))
 
