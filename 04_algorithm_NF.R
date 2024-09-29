f5f79472e97375abd1d9f44baedcee4496c83302
nefc42 <32312170+nefc42@users.noreply.github.com>
2024-09-29 18:00:40 +1300

    corrected some typos

--- 04_algorithm_NF.R
+++ 04_algorithm_NF.R
@@ -1,14 +1,4 @@
 
-# if data were individual numbers ...
-convert_to_population <- function(sprint, vodafone, num_adults, num_kids) {
-
-  # set c3 as mean of the other two:
-  third_provider = mean(sprint,vodafone)
-  population = (sprint + vodafone + third_provider) * (1.2 * num_adults + 0.1 * num_kids )
-
-    return(population)
-}
-
 #------------------------------------------------------------------------------
 # now write in dyplr for df manipulation
 
@@ -16,10 +6,11 @@ convert_to_population <- function(sprint, vodafone, num_adults, num_kids) {
 
 convert_to_population <- function(df_midnight) {
 
-  midnight_population %>% df_midnight
+  midnight_population <- df_midnight %>%
     mutate(third_provider = (spark_devices + vodafone_devices) / 2) %>%
     mutate(adult_count = (total_count - child_count)) %>%
-    mutatue(population = (sprint_devices + vodafone_devices + third_provider * (1.2 * adult_count + 0.1 * child_count)))
+    mutate(population = (spark_devices + vodafone_devices + third_provider) *
+              (1.2 * adult_count + 0.1 * child_count))
 
   return(midnight_population)
 }
